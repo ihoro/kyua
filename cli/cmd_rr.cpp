c3f440992a0c4ab9b0b710227e5cac312100dd99
Igor Ostapenko <pm@igoro.pro>
2024-12-14 21:35:41 +0000

    Add the same --kyuafile and --build-root options to kyua rr command as of kyua test

--- cli/cmd_rr.cpp
+++ cli/cmd_rr.cpp
@@ -30,6 +30,7 @@
 
 #include "cli/common.ipp"
 #include "engine/rr/rr.hpp"
+#include "utils/cmdline/options.hpp"
 #include "utils/format/macros.hpp"
 
 namespace cmdline = utils::cmdline;
@@ -43,8 +44,8 @@ using cli::cmd_rr;
 cmd_rr::cmd_rr(void) : cli_command(
     "rr", "[resolver-name ...]", 0, -1, "Run requirement resolvers")
 {
-    //add_option(build_root_option);
-    //add_option(kyuafile_option);
+    add_option(build_root_option);
+    add_option(kyuafile_option);
 }
 
 
